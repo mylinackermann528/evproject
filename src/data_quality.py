c572028815a283bdebe272d6941a3545b8ae3491
mylinackermann528 <mylin.ackermann528@gmail.com>
2025-10-16 15:40:59 -0500

    Update data_quality.py

--- src/data_quality.py
+++ src/data_quality.py
@@ -17,21 +17,21 @@ def run_dq_checks(df: DataFrame) -> dict:
 
     # Check 1: Count of null VINs
     if "VIN" in available_columns:
-        results["null_vin_count"] = df.where(col("VIN").isNull()).count()
+        results["null_vin_count"] = df.where(col("VIN_1_10").isNull()).count()
     else:
         print("DQ Warning: 'VIN' column not found, skipping null count check.")
         results["null_vin_count"] = -1 # Use -1 to indicate the check was skipped
 
     # Check 2: Count of records with a Base MSRP of 0
     if "BASEMSRP" in available_columns:
-        results["zero_msrp_count"] = df.where(col("BaseMSRP") == 0).count()
+        results["zero_msrp_count"] = df.where(col("BASE_MSRP") == 0).count()
     else:
         print("DQ Warning: 'BaseMSRP' column not found, skipping zero MSRP check.")
         results["zero_msrp_count"] = -1
 
     # Check 3: Count of records with an invalid Model Year
     if "MODELYEAR" in available_columns:
-        results["invalid_year_count"] = df.where(col("ModelYear") > 2025).count()
+        results["invalid_year_count"] = df.where(col("MODEL_YEAR") > 2025).count()
     else:
         print("DQ Warning: 'ModelYear' column not found, skipping invalid year check.")
         results["invalid_year_count"] = -1
