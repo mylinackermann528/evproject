b83ebd620a6793d36e9a9d9bf80c02a97a2e0bfd
mylinackermann528 <mylin.ackermann528@gmail.com>
2025-10-17 12:46:38 -0500

    Update transformations.py

--- src/aws/transformations.py
+++ src/aws/transformations.py
@@ -1,17 +1,8 @@
-from pyspark.sql.functions import col, explode
+from pyspark.sql.functions import col
 from pyspark.sql import DataFrame
 
 def clean_ev_data(df: DataFrame, columns: list) -> DataFrame:
-    if "data" in df.columns:
-        df_exploded = df.select(explode(col("data")).alias("record"))
-    else:
-        df_exploded = df.alias("record")
-
-    select_expressions = []
-    for i, column_name in enumerate(columns):
-        select_expressions.append(col(f"record.{i}").alias(column_name))
-        
-    final_df = df_exploded.select(select_expressions)
+    final_df = df
 
     final_df = final_df.withColumn("Model_Year", col("Model_Year").cast("integer")) \
                        .withColumn("Electric_Range", col("Electric_Range").cast("integer"))
