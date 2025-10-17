a7f441c5850e8967ca1f20673bbcca287ebd16f4
mylinackermann528 <mylin.ackermann528@gmail.com>
2025-10-17 12:52:43 -0500

    Update transformations.py

--- src/aws/transformations.py
+++ src/aws/transformations.py
@@ -1,14 +1,21 @@
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, explode
 from pyspark.sql import DataFrame
 
-def clean_ev_data(df: DataFrame, columns: list) -> DataFrame:
-    final_df = df
+def clean_ev_data(df: DataFrame) -> DataFrame:
+    column_metadata = df.select("meta.view.columns").first()[0]
+    column_names = [field['fieldName'] for field in column_metadata]
 
-    final_df = final_df.withColumn("Model_Year", col("Model_Year").cast("integer")) \
-                       .withColumn("Electric_Range", col("Electric_Range").cast("integer"))
+    df_exploded = df.select(explode(col("data")).alias("record"))
+
+    select_expressions = [col("record")[i].alias(column_names[i]) for i in range(len(column_names))]
+    final_df = df_exploded.select(select_expressions)
+
+    final_df = final_df.withColumn("model_year", col("model_year").cast("integer")) \
+                       .withColumn("electric_range", col("electric_range").cast("integer")) \
+                       .withColumn("base_msrp", col("base_msrp").cast("integer")) \
+                       .withColumn("legislative_district", col("legislative_district").cast("integer")) \
+                       .withColumn("dol_vehicle_id", col("dol_vehicle_id").cast("integer"))
                        
-    final_df = final_df.filter(col("VIN_1_10").isNotNull())
-
-    final_df = final_df.select(*columns)
+    final_df = final_df.filter(col("vin_1_10").isNotNull())
 
     return final_df
