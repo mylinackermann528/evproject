dc9fb05dc4c4353c2befed9cfb9e2f373c247b24
mylinackermann528 <mylin.ackermann528@gmail.com>
2025-10-17 12:53:02 -0500

    Update main.py

--- src/aws/main.py
+++ src/aws/main.py
@@ -3,26 +3,18 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql.functions import lit
 
 from transformations import clean_ev_data
 
 S3_RAW_PATH = "s3://ev-project-mylinackermann/bronze/ev_data_raw/"
 S3_SILVER_PATH = "s3://ev-project-mylinackermann/curated/ev_data_clean/"
 
-RDS_HOST = "ev-population-db.c0nsiackq1aq.us-east-1.rds.amazonaws.com"
+RDS_HOST = "ev-population-db.c0nsiackq1aq.us-east-1.rds.amazonaws.com" 
 RDS_PORT = "5432"
 RDS_DB = "ev-population-db"
 RDS_USER = "evprojectuser"
 RDS_PASSWORD = "NewMan2025!!!"
 RDS_TABLE = "ev_population_master"
-
-COLUMN_NAMES = [
-    "VIN_1_10", "County", "City", "State", "Postal_Code", "Model_Year",
-    "Make", "Model", "Electric_Vehicle_Type", "CAFV_Eligibility",
-    "Electric_Range", "Base_MSRP", "Legislative_District", "DOL_Vehicle_ID",
-    "Vehicle_ID", "2020_Census_Tract"
-]
 
 def main():
     args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -40,12 +32,11 @@ def main():
         raise Exception(f"Input file is empty or path is incorrect: {S3_RAW_PATH}")
 
     print("Starting data transformation and cleaning...")
-    clean_df = clean_ev_data(raw_df, COLUMN_NAMES)
+    clean_df = clean_ev_data(raw_df)
     
     print(f"Transformation complete. Writing {clean_df.count()} records.")
     
     print(f"Writing master data to S3 Delta Lake: {S3_SILVER_PATH}")
-    
     clean_df.write.format("delta").mode("overwrite").save(S3_SILVER_PATH)
 
     print(f"Writing master data to RDS table: {RDS_TABLE}")
